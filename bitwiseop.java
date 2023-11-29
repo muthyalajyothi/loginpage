03a664159fa7bc78364a68d101a38e67baee3813
jyothi <muthyalajyothi19gmail.com>
2023-11-29 23:47:53 +0530

    added a new bitwiseop.java

---
+++ bitwiseop.java
@@ -0,0 +1,19 @@
+public class bitwiseop {
+    public static void main(String args[]) {
+        int num1 = 11;
+        int num2 = 22; 
+        int result = 0;
+        result = num1 & num2;
+        System.out.println("num1 & num2: "+result);
+        result = num1 | num2;
+        System.out.println("num1 | num2: "+result);
+        result = num1 ^ num2;
+        System.out.println("num1 ^ num2: "+result);
+        result = ~num1;
+        System.out.println("~num1: "+result);
+        result = num1 << 2;
+        System.out.println("num1 << 2: "+result);
+         result = num1 >> 2;
+        System.out.println("num1 >> 2: "+result);
+    }
+ }
